4089b8e8df3c825b0aac40f94480a0326bf165f3
Bryan Freeman <bryan.freeman@metzohanian.com>
2018-01-19 15:41:35 -0600

    Exercise 04.01: AddNewActivity complete

---
+++ T04a.01-Exercise-AddNewActivity/app/src/main/java/com/example/android/explicitintent/ChildActivity.java
@@ -0,0 +1,20 @@
+package com.example.android.explicitintent;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class ChildActivity extends AppCompatActivity {
+
+    private TextView tvDisplay;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_child);
+
+        tvDisplay = (TextView) findViewById(R.id.tv_display);
+    }
+}
